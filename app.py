924f9d10b40c9c70c46f5d5ffae75986be24e60a
Ollie Fowlds <olliefowlds@gmail.com>
2025-11-17 22:15:54 +0000

    all before i add db

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, url_for, session, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+
+    if request.method == "POST":
+        topicSelect = request.form.get("topicSelect")
+        print(topicSelect)
+
     return render_template("home.html")
 
 if __name__ == "__main__":
